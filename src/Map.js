16ec2a262bb4073e7853c72046a4a70076099f67
Alex <griffiths.alex00@gmail.com>
2023-07-04 11:53:55 +0100

    map showing correctly

--- src/Map.js
+++ src/Map.js
@@ -4,6 +4,39 @@ import { ComposableMap, Geographies, Geography, Marker} from "react-simple-maps"
 const geoUrl =
   "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json"
 
+
+// class MapChart extends React.Component {
+
+//   constructor(props) {
+//     super(props)
+   
+//   }
+
+//   render() {
+//     return (
+//       <ComposableMap>
+//         <Geographies geography={geoUrl}>
+//           {({ geographies }) =>
+//             geographies.map((geo) => (
+//               <Geography
+//                   key={geo.rsmKey}
+//                   geography={geo}
+//                   fill="#DDD"
+//                   stroke="#FFF"
+//                   />
+//             ))
+//           }
+//         </Geographies>
+//         <Marker coordinates={[props.longitude, props.latitude]}>
+//           <circle r={8} fill="#F53" />
+//         </Marker>
+//       </ComposableMap>
+//     )
+//   }
+
+// }
+
+  
 function MapChart(props) {
   return (
     <ComposableMap>
